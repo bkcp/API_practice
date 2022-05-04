9f5585d42313ebafdd1565b025f118c43cc74196
bkcp <bkcp83@gmail.com>
2022-05-04 05:57:09 -0700

    XHR request

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log('How to work with APIs')
+console.log("How to work with APIs");
+
+//OLD WAY to send request XML  XHR
+//Doesnt support Promises
+
+// "http://swapi.dev/api/people/1/"
+
+const req = new XMLHttpRequest();
+
+req.onload = function () {
+  console.log("LOADED");
+  const data = JSON.parse(this.responseText);
+  console.log(data.birth_year, data.gender);
+};
+req.onerror = function () {
+  console.log("ERROR");
+  console.log(this);
+};
+
+req.open("GET", "http://swapi.dev/api/people/1/");
+req.send();
